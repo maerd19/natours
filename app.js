f3f59bfae5932900fa53b56f4f65947f71df1913
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-08-08 10:50:17 -0500

    Handling POST requests

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 
 const app = express()
 
+// Middleware to add the data from the body to the request object
+app.use(express.json())
+
 // app.get('/', (req, res) => {
 //     res
 //         .status(200)
@@ -22,6 +25,23 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: newId }, req.body)
+
+    tours.push(newTour)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 })
 
