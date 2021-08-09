391cb622895f1ed05fbf1b891fc9e19dccdbf95f
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-08-09 08:44:18 -0500

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(DB, {
     useCreateIndex: true,
     useFindAndModify: false
 }).then(con => {
-    console.log(con.connection)
+    // console.log(con.connection)
     console.log('DB connection successful!')
 })
 
@@ -31,6 +31,17 @@ const tourSchema = new mongoose.Schema({
     }
 })
 const Tour = mongoose.model('Tour', tourSchema)
+
+const testTour = new Tour({
+    name: 'The Park Camper',
+    price: 997
+})
+
+testTour.save().then(doc => {
+    console.log(doc)
+}).catch(err => {
+    console.log('ERROR: ', err)
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
