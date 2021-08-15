64349815510c10987b3b5ebbdf00c0f60c62b297
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-08-15 11:00:35 -0500

    Adding 404 Not Found Errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel')
 const APIFeatures = require('./../utils/apiFeatures')
 const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
 
 exports.aliasTopTours = (req, res, next) => {
     req.query.limit = 5
@@ -29,7 +30,15 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 })
 
 exports.getTour = catchAsync(async (req, res, next) => {
-    const tour = await Tour.findById(req.params.id)
+    const tour = await Tour.findById(req.params.id, err => {
+        if (err) {
+            return next(new AppError('No tour found with that ID', 404))
+        }
+    })
+    
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
     
     res.status(200).json({
         status: 'success',
@@ -54,7 +63,15 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true
+    }, err => {
+        if (err) {
+            return next(new AppError('No tour found with that ID', 404))
+        }
     })
+
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
 
     res.status(200).json({
         status: 'success',
@@ -65,7 +82,15 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 })
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-    await Tour.findByIdAndDelete(req.params.id)
+    const tour = await Tour.findByIdAndDelete(req.params.id, err => {
+        if (err) {
+            return next(new AppError('No tour found with that ID', 404))
+        }
+    })
+
+    if (!tour === false) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
 
     res.status(204).json({
         status: 'success',
