8d3e37d06f8ab0775970bd7d615a73e81c8d5e7a
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-08-15 09:41:21 -0500

    Better Error and Refactoring

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
@@ -23,27 +25,10 @@ app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on this server!`
-    // })
-
-    const err = new Error(`Can't find ${req.originalUrl} on this server!`)
-    err.statusCode = 404
-    err.status = 'fail'
-
-    next(err)
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || 'error'
-    
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-})
+app.use(globalErrorHandler)
 
 // 4) START SERVER
 module.exports = app
