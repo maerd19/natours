fef9cb5be0bae4f9fe3b7d3758b024fcdf9ccf07
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-08-15 20:04:25 -0500

    Handling Mongoose Validation Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,7 +8,15 @@ const handleCastErrorDB = err => {
 const handleDuplicateFieldsDB = err => {
     const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
     console.log(value)
+
     const message = `Duplicate field value: ${value}. Please use another value!`
+    return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message)    
+
+    const message = `Invalid input data. ${errors.join('. ')}`
     return new AppError(message, 400)
 }
 
@@ -55,6 +63,7 @@ module.exports = (err, req, res, next) => {
 
         if (error.name === 'CastError') error = handleCastErrorDB(error)
         if (error.code === 11000) error = handleDuplicateFieldsDB(error)
+        if (error.name === 'ValidationError') error = handleValidationErrorDB(error)
         
         sendErrorProd(error, res)
     }    
