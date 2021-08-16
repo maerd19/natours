e3021eb0537466ac68d2249ab679d006cb6bb218
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-08-15 20:41:13 -0500

    Errors Outside Express: Unhandled Rejection

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ mongoose.connect(DB, {
 })
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`)
+})
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message)
+    console.log('UNHANDLED REJECTION! Shutting down...')
+    server.close(() => {
+        process.exit(1)
+    })    
 })
