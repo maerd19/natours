ee9c6ef5890f85fd5a0d1ac453d1a8c8702b3680
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-08-24 08:28:18 -0500

    Preventing Parameter Pollution

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const userRouter = require('./routes/userRoutes')
 const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const app = express()
 
@@ -23,7 +24,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Limit requests from same API
 const limiter = rateLimit({
-    max: 3,
+    max: 100,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour'
 })
@@ -37,6 +38,18 @@ app.use(mongoSanitize())
  
 // Data sanitization against XSS
 app.use(xss())
+
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist: [
+        'duration',
+        'ratingsAverage',
+        'ratingsQuantity',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}))
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`))
