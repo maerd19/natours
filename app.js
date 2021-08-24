d6fc4cf2bc746c8a97430adb7deba1232b2c122b
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-08-24 06:59:11 -0500

    Setting Security HTTP Methods

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
+const helmet = require('helmet')
 
 const app = express()
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet())
+
+// Development loggin
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
     max: 3,
     windowMs: 60 * 60 * 1000,
@@ -21,12 +27,16 @@ const limiter = rateLimit({
 })
 app.use('/api', limiter)
 
-app.use(express.json())
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }))
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`))
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
-
+    // console.log(req.headers)
     next()
 })
 
