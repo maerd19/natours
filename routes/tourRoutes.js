275f1c4378c174532239dde523b4dc9e9d1f772f
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-09-05 12:26:46 -0500

    Geospatial Queries: Finding Tours Within Radius

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,6 +13,7 @@ router
     .route('/top-5-cheap')
     .get(tourController.aliasTopTours, tourController.getAllTours)
 
+router.route('/tour-stats').get(tourController.getTourStats)
 router
     .route('/monthly-plan/:year')
     .get(authController.protect,
@@ -21,8 +22,10 @@ router
     )
 
 router
-    .route('/tour-stats')
-    .get(tourController.getTourStats)
+    .route('/tours-within/:distance/center/:latlng/unit/:unit')
+    .get(tourController.getToursWithin)
+// /tours-distance?distance=233&center=-40,45&unit=mi
+// /tours-distance/233/center/-40,45/unit/mi
 
 router
     .route('/')
