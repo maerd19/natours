4458b2e22cf119bb242c70e4ad6ba6b9e187a7dd
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-09-12 17:00:03 -0500

    Setting up Pug in Express

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
@@ -12,10 +13,17 @@ const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
 
-
 const app = express()
 
+// Tell Express what template engine we are going to use
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
 // 1) GLOBAL MIDDLEWARES
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 // Set security HTTP headers
 app.use(helmet())
 
@@ -53,9 +61,6 @@ app.use(hpp({
     ]
 }))
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`))
-
 // Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
@@ -64,6 +69,10 @@ app.use((req, res, next) => {
 })
 
 // 3) ROUTES
+app.get('/', (req, res) => {
+    res.status(200).render('base')
+})
+
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/reviews', reviewRouter)
