8dce3e15df4be5b574b8be66436c7b714f4f2c25
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-09-26 12:16:03 -0500

    Logging out Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,6 +47,16 @@ exports.signup = catchAsync(async (req, res, next) => {
     createSendToken(newUser, 201, res)
 })
 
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    })
+    res.status(200).json({
+        status: 'success'
+    })
+}
+
 exports.login = catchAsync(async (req, res, next) => {
     const { email, password } = req.body
 
@@ -98,31 +108,35 @@ exports.protect = catchAsync(async (req, res, next) => {
 })
 
 // Only for rendered pages, no errors!
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
     if (req.cookies.jwt) {
-        // 1) Verify token
-        const decoded = await promisify(jwt.verify)(
-            req.cookies.jwt,
-            process.env.JWT_SECRET, (req, res)
-        )
+        try {
+            // 1) Verify token
+            const decoded = await promisify(jwt.verify)(
+                req.cookies.jwt,
+                process.env.JWT_SECRET, (req, res)
+            )
 
-        // 2) Check if user still exists
-        const currentUser = await User.findById(decoded.id)
-        if (!currentUser) {
+            // 2) Check if user still exists
+            const currentUser = await User.findById(decoded.id)
+            if (!currentUser) {
+                return next()
+            }
+
+            // 3) Check if user changed password after JWT was issued
+            if (currentUser.changePasswordAfter(decoded.iat)) {
+                return next()
+            }
+            
+            // THERE IS A LOGGED IN USER
+            res.locals.user = currentUser
+            return next()
+        } catch (err) {
             return next()
         }
-
-        // 3) Check if user changed password after JWT was issued
-        if (currentUser.changePasswordAfter(decoded.iat)) {
-            return next()
-        }
-        
-        // THERE IS A LOGGED IN USER
-        res.locals.user = currentUser
-        return next()
     }
     next()
-})
+}
 
 // Since we cannot pass arguments into a MW we can use a wrapper function in order to send the parameters into the MW
 exports.restrictTo = (...roles) => {
