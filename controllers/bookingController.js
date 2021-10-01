849c97125db241ddac558e00a09848b2e51fa27c
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-10-01 10:07:36 -0500

    Finishing the Bookings API

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const Tour = require('../models/tourModel')
 const Booking = require('../models/bookingModel')
+const factory = require('./handlerFactory')
 const catchAsync = require('../utils/catchAsync')
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
@@ -45,3 +46,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
     res.redirect(req.originalUrl.split('?')[0])
 })
+
+exports.createBooking = factory.createOne(Booking)
+exports.getBooking = factory.getOne(Booking)
+exports.getAllBookings = factory.getAll(Booking)
+exports.updateBooking = factory.updateOne(Booking)
+exports.deleteBooking = factory.deleteOne(Booking)
