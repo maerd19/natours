a6c0483084b4f3f795fe8c7f8bb8bf18ab43cf16
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2021-10-21 11:21:55 -0500

    Implemented CORS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
+const cors = require('cors')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -27,6 +28,12 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors())
+// Access-Control-Allow-Origin *
+
+app.options('*', cors())
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')))
 
